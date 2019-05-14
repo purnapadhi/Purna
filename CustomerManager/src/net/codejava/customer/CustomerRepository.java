66ee4cf1a7b50609f60169455d544fb91f788602
SG0305760 <SG0305760@DEHNRCWT2.iamsabre.sabrenow.com>
2019-05-15 02:06:55 +0530

    refactor my classess

--- CustomerManager/src/net/codejava/customer/CustomerRepository.java
+++ CustomerManager/src/net/codejava/customer/CustomerRepository.java
@@ -11,9 +11,9 @@ public class CustomerRepository {
 	
 	public static List<Customer> m1() {
 		List<Customer> customerList=new ArrayList<Customer>();
-				Customer cust=new Customer(100L,"RAM", "RAM@gmail.com"," RAM Colony");
-				Customer cust1=new Customer(300L,"RAM1", "RAM1@gmail.com"," RAM1 Colony");
-				Customer cust2=new Customer(200L,"RAM2", "RAM2@gmail.com"," RAM2 Colony");
+				Customer cust=new Customer(1000L,"RAM", "RAM@gmail.com"," RAM Colony");
+				Customer cust1=new Customer(1001L,"RAM1", "RAM1@gmail.com"," RAM1 Colony");
+				Customer cust2=new Customer(1002L,"RAM2", "RAM2@gmail.com"," RAM2 Colony");
 				customerList.add(cust);
 				customerList.add(cust1);
 				customerList.add(cust2);
